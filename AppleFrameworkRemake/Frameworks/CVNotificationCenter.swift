44f9fb30caf0e639d61edf3d404f269a60f0d20f
Cuong Vuong <cuongv88@gmail.com>
2019-06-12 17:35:20 +0800

    Initial commit.c

---
+++ AppleFrameworkRemake/Frameworks/CVNotificationCenter.swift
@@ -0,0 +1,68 @@
+//
+//  CVNotificationCenter.swift
+//  AppleFrameworkRemake
+//
+//  Created by Cuong Vuong on 12/6/19.
+//  Copyright © 2019 Cuong Vuong. All rights reserved.
+//
+
+import Foundation
+
+fileprivate struct CVDictionaryKey: Hashable {
+    let observer: NSObject
+    let name: Notification.Name
+}
+
+fileprivate extension Dictionary where Key == CVDictionaryKey, Value ==  Selector {
+    func getObserversAndSelectors(for name: Notification.Name) -> [(NSObject, Selector)] {
+        return keys.filter { $0.name == name }.map { ($0.observer, self[$0]!) }
+    }
+    
+    mutating func removeAllSelectors(for observer: NSObject) {
+        keys.filter { $0.observer == observer }.forEach { self[$0] = nil }
+    }
+    
+    mutating func removeSelector(for observer: NSObject, and name: Notification.Name) {
+        if let key = keys.first(where: { $0.observer == observer && $0.name == name }) {
+            self[key] = nil
+        }
+    }
+}
+
+public final class CVNotificationCenter {
+    static let `default` = CVNotificationCenter()
+    
+    private let queue = DispatchQueue(label: "CVNotificationQueue")
+    private var observersDictionary = [CVDictionaryKey: Selector]()
+    
+    func addObserver(_ observer: NSObject, selector: Selector, name: Notification.Name) {
+        queue.async(flags: .barrier) { [weak self] in
+            let key = CVDictionaryKey(observer: observer, name: name)
+            self?.observersDictionary[key] = selector
+        }
+    }
+    
+    func post(_ notification: Notification) {
+        queue.async { [weak self] in
+            guard let obsAndSels = self?.observersDictionary.getObserversAndSelectors(for: notification.name) else { return }
+            
+            obsAndSels.forEach { (observer, selector) in
+                if observer.responds(to: selector) {
+                    observer.perform(selector, with: notification)
+                }
+            }
+        }
+    }
+    
+    func removeObserver(_ observer: NSObject) {
+        queue.async(flags: .barrier) { [weak self] in
+            self?.observersDictionary.removeAllSelectors(for: observer)
+        }
+    }
+    
+    func removeObserver(_ observer: NSObject, for name: Notification.Name) {
+        queue.async(flags: .barrier) { [weak self] in
+            self?.observersDictionary.removeSelector(for: observer, and: name)
+        }
+    }
+}
---
+++ AppleFrameworkRemakeTests/AppleFrameworkRemakeTests.swift
@@ -0,0 +1,41 @@
+//
+//  AppleFrameworkRemakeTests.swift
+//  AppleFrameworkRemakeTests
+//
+//  Created by Cuong Vuong on 12/6/19.
+//  Copyright © 2019 i3. All rights reserved.
+//
+
+import XCTest
+@testable import AppleFrameworkRemake
+
+class AppleFrameworkRemakeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+//    func testCVNotificationCenter() {
+//        let notification = Notification(name: .init("notification"), object: self, userInfo: ["data": 1])
+//        CVNotificationCenter.default.addObserver(self, selector: #selector(gotNotification), name: .init("notification"))
+//        CVNotificationCenter.default.post(notification)
+//        CVNotificationCenter.default.removeObserver(self)
+//        CVNotificationCenter.default.post(notification)
+//    }
+//
+//    @objc private func gotNotification(noti: Notification) {
+//
+//    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
